8db1da6cf7af75d8c18a957dac2aaea4bd9b467a
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-02-10 14:35:31 +0100

    Add some snippets for maude and gohtmltmpl

---
+++ lua/falkjet/snippets/gohtmltmpl.lua
@@ -0,0 +1,24 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require 'luasnip.extras.fmt'.fmt
+local fmta = require 'luasnip.extras.fmt'.fmta
+local rep = require 'luasnip.extras'.rep
+
+
+
+return {
+  s({ trig = 'def' }, fmta([[
+    {{ define "<>" }}
+        <>
+    {{ end }}
+  ]], { i(1), i(0) })),
+  s({ trig = 'tm' }, fmta([[
+    {{ template "<>" <> }}
+  ]], { i(1), i(2, '.') })),
+}
---
+++ lua/falkjet/snippets/maude.lua
@@ -0,0 +1,45 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local fmt = require 'luasnip.extras.fmt'.fmt
+local fmta = require 'luasnip.extras.fmt'.fmta
+local rep = require 'luasnip.extras'.rep
+
+function pattern_condition(pattern)
+  return function(line_to_cursor)
+    return string.match(line_to_cursor, pattern)
+  end
+end
+
+return {
+  s({ trig = 'fmod' }, fmta([[
+    fmod <> is
+        <>
+    endfm
+  ]], { i(1), i(0) })),
+  s({
+    trig = 'var ',
+    snippetType = 'autosnippet',
+    condition = pattern_condition '^%s*var $',
+  }, fmta([[ var <> . ]], { i(0) })),
+  s({
+    trig = 'vars ',
+    snippetType = 'autosnippet',
+    condition = pattern_condition '^%s*vars $',
+  }, fmta([[ vars <> . ]], { i(0) })),
+  s({
+    trig = 'eq ',
+    snippetType = 'autosnippet',
+    condition = pattern_condition '^%s*eq $',
+  }, fmta([[ eq <> . ]], { i(0) })),
+  s({
+    trig = 'op ',
+    snippetType = 'autosnippet',
+    condition = pattern_condition '^%s*op $',
+  }, fmta([[ op <> . ]], { i(0) })),
+}
