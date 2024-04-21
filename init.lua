3fc61a7da1bf083e40a8181742d279ff744321f5
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-04-21 10:28:59 +0200

    Disable vimtex key mappings

--- init.lua
+++ init.lua
@@ -54,6 +54,7 @@ vim.g.vim_markdown_frontmatter = 1
 vim.g.vim_markdown_toml_frontmatter = 1
 vim.g.vim_markdown_math = 1
 
+vim.g.vimtex_mappings_enabled = 0
 vim.g.tex_flavor = 'latex'
 vim.g.vimtex_view_method = 'zathura'
 vim.g.vimtex_quickfix_mode = 1
