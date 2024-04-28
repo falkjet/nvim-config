6bb4b74e7ce71037492880e813e4d676b87df1e4
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-04-28 17:32:35 +0200

    Fix go injection query for sql

--- queries/go/injections.scm
+++ queries/go/injections.scm
@@ -1,6 +1,11 @@
-(call_expression function:
- (selector_expression
-  field: (field_identifier)@_field
-         (#match? @_field "^(Queryx?|(Must)?Exec(ute)?)$"))
-  arguments: (argument_list
-               (raw_string_literal) @sql))
+(call_expression
+  function:
+  (selector_expression
+    field: (field_identifier)@_field
+    (#match? @_field "^(Queryx?|(Must)?Exec(ute)?)$"))
+  arguments:
+  (argument_list
+    (raw_string_literal)
+    @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "sql")))
