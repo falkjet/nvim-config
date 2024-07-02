891739e06a293cfb021ec61f2cfb78cbef1beeee
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-07-02 17:54:50 +0200

    Better autocomplete keybinds

--- lua/falkjet/autocomplete.lua
+++ lua/falkjet/autocomplete.lua
@@ -13,23 +13,23 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      -- select = true,
-    },
     ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
+      if luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
     end, { 'i', 's' }),
+    ['<c-y>'] = cmp.mapping.confirm {
+      select = true,
+      behavior = cmp.ConfirmBehavior.Replace
+    },
+    ['<c-k>'] = cmp.mapping.confirm {
+      select = true,
+      behavior = cmp.ConfirmBehavior.Replace
+    },
     ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
