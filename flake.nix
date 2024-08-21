1ea51a0965427aa56496783fd4386093ba630bca
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-08-21 13:31:46 +0200

    Reformat flake.nix

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
     };
   };
   outputs = { self, nixpkgs, neovim-nightly-overlay }:
-    let pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config.allowUnfree = true;
-      overlays = [ neovim-nightly-overlay.overlay ];
-    };
-    in
-    {
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+        overlays = [ neovim-nightly-overlay.overlay ];
+      };
+    in {
       packages.x86_64-linux.neovim = import ./nvim.nix { inherit pkgs; };
       packages.x86_64-linux.default = self.packages.x86_64-linux.neovim;
     };
