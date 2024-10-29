35f1b1c42657a02b7e5c68ed002dd6ef1128e3e4
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-10-29 19:19:34 +0100

    Add nu treesitter grammar

--- lua/falkjet/treesitter.lua
+++ lua/falkjet/treesitter.lua
@@ -1,3 +1,13 @@
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.nu = {
+  install_info = {
+    url = "https://github.com/nushell/tree-sitter-nu",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "nu",
+}
+
 vim.defer_fn(function()
   ---@diagnostic disable-next-line: missing-fields
   require('nvim-treesitter.configs').setup {
