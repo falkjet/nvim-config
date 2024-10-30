a4d190536cb5e02b95a86f207f20135a09c55442
Falk Markus Dursun Jetlund <falk@jetlund.com>
2024-10-30 19:08:28 +0100

    Reformat snippets for lua

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -1,37 +1,54 @@
-local ls = require 'luasnip'
-local s = ls.snippet
-local sn = ls.snippet_node
-local t = ls.text_node
-local i = ls.insert_node
-local f = ls.function_node
-local d = ls.dynamic_node
-local fmt = require 'luasnip.extras.fmt'.fmt
-local fmta = require 'luasnip.extras.fmt'.fmta
-local rep = require 'luasnip.extras'.rep
-
 local function get_visual(_, parent)
-  if #parent.snippet.env.LS_SELECT_RAW > 0 then
-    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
-  else
-    return sn(nil, i(1))
-  end
+	if #parent.snippet.env.LS_SELECT_RAW > 0 then
+		return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+	else
+		return sn(nil, i(1))
+	end
 end
 
 local function spaceif(args)
-  return args[1][1] == '' and '' or ' '
+	return args[1][1] == '' and '' or ' '
 end
 
-
+local function get_cursor_0ind()
+	local c = vim.api.nvim_win_get_cursor(0)
+	c[1] = c[1] - 1
+	return c
+end
 
 return {
-  s({ trig = "fun" }, fmta([[
+	-- function
+	s({ trig = "fun" }, fmta([[
     function<><>(<>)
       <>
     end
   ]], { f(spaceif, 1), i(1), i(2), i(0) })),
-  s({ trig = "fnl" }, fmta([[
+
+	-- Local function
+	s({ trig = "fnl" }, fmta([[
     local function <>(<>)
       <>
     end
-  ]], { i(1), i(2), i(0) }))
+  ]], { i(1), i(2), i(0) })),
+
+
+	-- Use end instead of {}
+	s({
+		trig = '(function.*[(].*[)]) {',
+		trigEngine = 'pattern',
+		wordTrig = false,
+		snippetType = 'autosnippet',
+		resolveExpandParams = function(snip, line, trig, capt)
+			local pos = get_cursor_0ind()
+			local line = vim.api.nvim_get_current_line()
+			return {
+				clear_region = {
+					from = { pos[1], 0 },
+					to = { pos[1], #line }
+				}
+			}
+		end
+	}, fmta('<> <>end', { f(function(_, snip)
+		return snip.captures[1]
+	end), i(0) }))
 }
