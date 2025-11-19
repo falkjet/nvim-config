48771e02f9cd749d7f7d5950d1c2d4c7aa4a2801
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-11-19 15:24:06 +0100

    Bunch of completion stuff

---
+++ fnl/config/plugins/autocomplete.fnl
@@ -0,0 +1,58 @@
+
+(fn cmp-opts []
+  (local cmp (require :cmp))
+  (local luasnip (require :luasnip))
+  (local m cmp.mapping)
+
+
+  ;; Emmet stuff
+  (fn make-set [& args]
+    (collect [_ val (ipairs args)] (values val true)))
+
+  (local emmet-filetypes
+    (make-set :html :eruby :vue :templ :heex :markdown))
+
+  (fn emmet-expandable? []
+    (and (. emmet-filetypes vim.o.ft)
+         (= (vim.fn.emmet#isExpandable) 1)))
+
+  (fn emmet-expand []
+    (vim.cmd "silent execute \"normal \\<Plug>(emmet-expand-abbr)\""))
+
+  ;; Use luasnip or emmet on <tab>
+  (fn on-tab [fallback]
+    (if
+      (luasnip.expand_or_locally_jumpable) (luasnip.expand_or_jump)
+      (emmet-expandable?) (emmet-expand)
+      (fallback)))
+
+  ;; Jump back on <s-tab>
+  (fn on-s-tab [fallback]
+    (if
+      (luasnip.locally_jumpable -1) (luasnip.jump -1)
+      (fallback)))
+
+  (local mappings
+    {"<C-n>" (m.select_next_item)
+     "<C-p>" (m.select_prev_item)
+     "<C-d>" (m.scroll_docs -4)
+     "<C-f>" (m.scroll_docs 4)
+     "<C-Space>" (m.complete {})
+     "<Tab>" (m on-tab [:i :s])
+     "<C-k>" (m.confirm {:select true
+			 :behavior cmp.ConfirmBehavior.Replace})
+     "<S-Tab>" (m on-s-tab [:i :s])})
+
+  {:mapping (m.preset.insert mappings)
+   :sources [{:name :buffer}
+	     {:name :conjure}
+	     {:name :tags}]})
+
+[:hrsh7th/cmp-buffer
+ :hrsh7th/cmp-nvim-lsp
+ :PaterJason/cmp-conjure
+ :quangnguyen30192/cmp-nvim-tags
+ :mattn/emmet-vim
+ {1 :L3MON4D3/luasnip}
+ {1 :hrsh7th/nvim-cmp
+  :opts cmp-opts}]
---
+++ lua/config/plugins/autocomplete.lua
@@ -0,0 +1,44 @@
+-- [nfnl] fnl/config/plugins/autocomplete.fnl
+local function cmp_opts()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local m = cmp.mapping
+  local function make_set(...)
+    local args = {...}
+    local tbl_21_ = {}
+    for _, val in ipairs(args) do
+      local k_22_, v_23_ = val, true
+      if ((k_22_ ~= nil) and (v_23_ ~= nil)) then
+        tbl_21_[k_22_] = v_23_
+      else
+      end
+    end
+    return tbl_21_
+  end
+  local emmet_filetypes = make_set("html", "eruby", "vue", "templ", "heex", "markdown")
+  local function emmet_expandable_3f()
+    return (emmet_filetypes[vim.o.ft] and (vim.fn["emmet#isExpandable"]() == 1))
+  end
+  local function emmet_expand()
+    return vim.cmd("silent execute \"normal \\<Plug>(emmet-expand-abbr)\"")
+  end
+  local function on_tab(fallback)
+    if luasnip.expand_or_locally_jumpable() then
+      return luasnip.expand_or_jump()
+    elseif emmet_expandable_3f() then
+      return emmet_expand()
+    else
+      return fallback()
+    end
+  end
+  local function on_s_tab(fallback)
+    if luasnip.locally_jumpable(-1) then
+      return luasnip.jump(-1)
+    else
+      return fallback()
+    end
+  end
+  local mappings = {["<C-n>"] = m.select_next_item(), ["<C-p>"] = m.select_prev_item(), ["<C-d>"] = m.scroll_docs(-4), ["<C-f>"] = m.scroll_docs(4), ["<C-Space>"] = m.complete({}), ["<Tab>"] = m(on_tab, {"i", "s"}), ["<C-k>"] = m.confirm({select = true, behavior = cmp.ConfirmBehavior.Replace}), ["<S-Tab>"] = m(on_s_tab, {"i", "s"})}
+  return {mapping = m.preset.insert(mappings), sources = {{name = "buffer"}, {name = "conjure"}, {name = "tags"}}}
+end
+return {"hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp", "PaterJason/cmp-conjure", "quangnguyen30192/cmp-nvim-tags", "mattn/emmet-vim", {"L3MON4D3/luasnip"}, {"hrsh7th/nvim-cmp", opts = cmp_opts}}
