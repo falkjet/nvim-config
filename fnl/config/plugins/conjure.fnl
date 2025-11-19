ba04a1f40dcf31fd875e628f27e1e67ca7c8b548
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-11-19 14:51:41 +0100

    Clojure jack-in and disable conjure for most filetypes

--- fnl/config/plugins/conjure.fnl
+++ fnl/config/plugins/conjure.fnl
@@ -1,4 +1,9 @@
-[{1 :Olical/conjure
+[{1 :clojure-vim/vim-jack-in
+  :dependencies [:vim-dispatch]
+  :cmd [:Boot :Clj :Lein]}
+ {1 :Olical/conjure
   :ft [:fennel :clojure]
+  :init (fn []
+          (set vim.g.conjure#filetypes [:clojure :fennel]))
   :keys [["<leader>er" "<cmd>ConjureEvalRootForm<cr>"]
 	 ["<leader>eb" "<cmd>ConjureEvalFile"]]}]
--- lua/config/plugins/conjure.lua
+++ lua/config/plugins/conjure.lua
@@ -1,2 +1,6 @@
 -- [nfnl] fnl/config/plugins/conjure.fnl
-return {{"Olical/conjure", ft = {"fennel", "clojure"}, keys = {{"<leader>er", "<cmd>ConjureEvalRootForm<cr>"}, {"<leader>eb", "<cmd>ConjureEvalFile"}}}}
+local function _1_()
+  vim.g["conjure#filetypes"] = {"clojure", "fennel"}
+  return nil
+end
+return {{"clojure-vim/vim-jack-in", dependencies = {"vim-dispatch"}, cmd = {"Boot", "Clj", "Lein"}}, {"Olical/conjure", ft = {"fennel", "clojure"}, init = _1_, keys = {{"<leader>er", "<cmd>ConjureEvalRootForm<cr>"}, {"<leader>eb", "<cmd>ConjureEvalFile"}}}}
