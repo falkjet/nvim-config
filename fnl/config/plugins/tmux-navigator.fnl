399e98a20a6403a95567b988e705d8c9133c8f5d
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-11-19 08:47:03 +0100

    Add vim tmux navigator

---
+++ fnl/config/plugins/tmux-navigator.fnl
@@ -0,0 +1,8 @@
+
+
+[{1 :christoomey/vim-tmux-navigator
+  :cmd [:TmuxNavigateLeft :TmuxNavigateDown :TmuxNavigateRight :TmuxNavigateUp]
+  :keys [{:mode [:n :i :t] 1 "<m-h>" 2 "<cmd>TmuxNavigateLeft<cr>" :desc "Left Pane"}
+	 {:mode [:n :i :t] 1 "<m-j>" 2 "<cmd>TmuxNavigateDown<cr>" :desc "Below Pane"}
+	 {:mode [:n :i :t] 1 "<m-k>" 2 "<cmd>TmuxNavigateUp<cr>" :desc "Above Pane"}
+	 {:mode [:n :i :t] 1 "<m-l>" 2 "<cmd>TmuxNavigateRight<cr>" :desc "Right Pane"}]}]
---
+++ lua/config/plugins/tmux-navigator.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/config/plugins/tmux-navigator.fnl
+return {{"christoomey/vim-tmux-navigator", cmd = {"TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateRight", "TmuxNavigateUp"}, keys = {{"<m-h>", "<cmd>TmuxNavigateLeft<cr>", mode = {"n", "i", "t"}, desc = "Left Pane"}, {"<m-j>", "<cmd>TmuxNavigateDown<cr>", mode = {"n", "i", "t"}, desc = "Below Pane"}, {"<m-k>", "<cmd>TmuxNavigateUp<cr>", mode = {"n", "i", "t"}, desc = "Above Pane"}, {"<m-l>", "<cmd>TmuxNavigateRight<cr>", mode = {"n", "i", "t"}, desc = "Right Pane"}}}}
