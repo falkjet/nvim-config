a1bac7da691704b540af8b026b8850f127dafec9
Falk Markus Dursun Jetlund <falk@jetlund.com>
2025-11-19 14:36:32 +0100

    Don't lazy load oil

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,2 +1,2 @@
 -- [nfnl] fnl/config/plugins/oil.fnl
-return {{"stevearc/oil.nvim", opts = {win_options = {wrap = false}, skip_confirm_for_simple_edits = true}, keys = {{"-", "<cmd>lua require 'oil'.open()<cr>"}}}}
+return {{"stevearc/oil.nvim", opts = {win_options = {wrap = false}, skip_confirm_for_simple_edits = true}, dependencies = {"nvim-tree/nvim-web-devicons"}, keys = {{"-", "<cmd>lua require 'oil'.open()<cr>"}}, lazy = false}}
