2aaf8b96412d8e6f4da3744c15a57ff147c1d3f9
Falk Markus Dursun Jetlund <falk@jetlund.com>
2026-09-18 19:16:15 +0200

    Add live-preview

---
+++ fnl/config/plugins/live-preview.fnl
@@ -0,0 +1,2 @@
+[{1 :brianhuster/live-preview.nvim
+  :dependencies [:folke/snacks.nvim]}]
---
+++ lua/config/plugins/live-preview.lua
@@ -0,0 +1,2 @@
+-- [nfnl] fnl/config/plugins/live-preview.fnl
+return {{"brianhuster/live-preview.nvim", dependencies = {"folke/snacks.nvim"}}}
