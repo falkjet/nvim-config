3fcca8e121f7097ffce4c9fffeace4abe4504c00
Falk Markus Dursun Jetlund <falk@jetlund.com>
2026-09-18 19:01:34 +0200

    Add snippet to write name and email of author based on git settings

--- luasnippets/all/misc.lua
+++ luasnippets/all/misc.lua
@@ -1,3 +1,15 @@
+function run(...)
+	local result = vim.system({...}, {text = true}):wait()
+	return result.stdout:match("(.*)\n")
+end
+
+run('git')
+
 return {
-	s('today', f(function() return vim.fn.strftime('%F') end))
+	s('today', f(function() return vim.fn.strftime('%F') end)),
+	s('me', f(function()
+		local name = run('git', 'config', 'user.name')
+		local email = run('git', 'config', 'user.email')
+		return name .. " <" .. email .. ">"
+	end)),
 }
