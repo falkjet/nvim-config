ce8a30d36ffd204a67abe4caf57f23d6f47b12ec
Falk Markus Dursun Jetlund <falk@jetlund.com>
2026-09-18 19:00:05 +0200

    Go banner snippet

--- luasnippets/go.lua
+++ luasnippets/go.lua
@@ -10,12 +10,22 @@ local function g(n)
 	end)
 end
 
+local function dashes(args)
+  return ("-"):rep(args[1][1]:len())
+end
 
 return {
 	auto('for (%a+) in (.*) {', 'for _, <> := range <> {', 1, 2),
 	auto('for (%a+), (%a+) in (.*) {', 'for <>, <> := range <> {', 1, 2, 3),
 	auto('struct (%a*) ', 'type <> struct ', 1, pattern '^%s*struct'),
 	auto('interface (%a*) ', 'type <> interface ', 1, pattern '^%s*interface'),
+	auto('(.*) := (.*)%.map (%a*): (.*) {', '<> := make([]<>, len(<>))\nfor <>, x := range <> {', 1, 4, 2, 3, 2),
+	auto('map (.*) [-]> (%a*) (.*) {', '<> := make([]<>, len(<>))\nfor i, x := range <> {', 2, 3, 1, 1),
 	s({ trig = '^(%s*)([^%s].*).app', trigEngine = 'pattern', hidden = true },
 		fmt('{1}{2} = append({2}, {3})', { g(1), g(2), i(1) })),
+	s('banner', fmta([[
+		//-<>-//
+		// <> //
+		//-<>-//
+	]], { f(dashes, 1), i(1), f(dashes, 1) })),
 }
