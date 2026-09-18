867a4403c7b06022aca226acd4db58c113155e4f
Falk Markus Dursun Jetlund <falk@jetlund.com>
2026-09-18 18:59:39 +0200

    Treesitter based indent for qmljs

---
+++ queries/qmljs/indents.scm
@@ -0,0 +1,9 @@
+(ui_object_initializer) @indent.begin
+(statement_block) @indent.begin
+(object) @indent.begin
+(array) @indent.begin
+(arguments) @indent.begin
+
+"}" @indent.branch @indent.end
+")" @indent.branch @indent.end
+"]" @indent.branch @indent.end
